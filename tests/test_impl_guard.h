93559d2628d1c3d1420cb5421b07818234c6b0f5
Gyeongtae Kim <codingpelican@gmail.com>
2025-01-09 22:06:27 +0900

    Remove test code before building library

--- tests/test_impl_guard.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_IMPL_GUARD_INCLUDED
-#define TEST_IMPL_GUARD_INCLUDED (1)
-
-
-void PrintColorChannel();
-
-
-#endif /* TEST_IMPL_GUARD_INCLUDED */
--- tests/test_impl_main.c
+++
@@ -1,9 +0,0 @@
-#include "test_impl_guard.h"
-#include <stdio.h>
-
-
-int main() {
-    printf("Hello, world!\n");
-    PrintColorChannel();
-    return 0;
-}
