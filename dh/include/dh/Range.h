35db249dd447bec2e10ed8dc24ca7d0a06760c43
Gyeongtae Kim <codingpelican@gmail.com>
2025-04-18 15:25:30 +0900

    feat: Enhance Range functionality with new prefix and suffix creation methods
    - Add `Range_prefix` function to create a Range from a specified end index (exclusive)
    - Add `Range_suffix` function to create a Range from a specified begin index (exclusive)

--- dh/include/dh/Range.h
+++ dh/include/dh/Range.h
@@ -5,8 +5,8 @@
  * @file    Range.h
  * @author  Gyeongtae Kim(dev-dasae) <codingpelican@gmail.com>
  * @date    2024-10-23 (date of creation)
- * @updated 2025-03-10 (date of last update)
- * @version v0.1-alpha.1
+ * @updated 2025-04-18 (date of last update)
+ * @version v0.1-alpha.2
  * @ingroup dasae-headers(dh)
  * @prefix  Range
  *
@@ -33,8 +33,12 @@ typedef struct Range {
     usize end;   ///< Ending index (exclusive)
 } Range;
 
-/// Create a Range from begin and end indices [begin..end] => [begin, end)
+/// Create a Range from begin and end indices (exclusive) [begin..end] => [begin, end)
 $inline_always fn_(Range_from(usize begin, usize end), Range);
+/// Create a Range from begin to specified end (exclusive) [begin..] => [begin, self.end)
+$inline_always fn_(Range_prefix(Range self, usize end), Range);
+/// Create a Range from specified begin to end (exclusive) [..end] => [self.begin, end)
+$inline_always fn_(Range_suffix(Range self, usize begin), Range);
 /// Get the length of a Range
 $inline_always fn_(Range_len(Range self), usize);
 /// Check if an index is within a Range
@@ -52,6 +56,14 @@ $inline_always fn_(Range_ne(Range lhs, Range rhs), bool);
 $inline_always fn_(Range_from(usize begin, usize end), Range) {
     debug_assert_fmt(begin < end, "Invalid range: begin(%zu) >= end(%zu)", begin, end);
     return (Range){ .begin = begin, .end = end };
+}
+$inline_always fn_(Range_prefix(Range self, usize end), Range) {
+    debug_assert_fmt(end > self.begin, "Invalid range: end(%zu) <= begin(%zu)", end, self.begin);
+    return (Range){ .begin = self.begin, .end = end };
+}
+$inline_always fn_(Range_suffix(Range self, usize begin), Range) {
+    debug_assert_fmt(begin < self.end, "Invalid range: begin(%zu) >= end(%zu)", begin, self.end);
+    return (Range){ .begin = begin, .end = self.end };
 }
 $inline_always fn_(Range_len(Range self), usize) { return self.end - self.begin; }
 $inline_always fn_(Range_contains(Range self, usize index), bool) { return self.begin <= index && index < self.end; }
