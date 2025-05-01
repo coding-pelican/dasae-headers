3a20119172163c9496850832134203ffaac9d8db
Gyeongtae Kim <codingpelican@gmail.com>
2025-05-01 14:14:18 +0900

    feat: Make member access operations valid after function calls inside `pipe`
    Enhance `pipe` macro implementation to support member access operations
    following function calls in `pipe`. This allows for direct field
    access and method chaining after function invocations within pipe
    expressions.
    
    Key changes:
    - Add support for dereferencing and member access operations in pipe steps
    - Improve argument handling to accommodate post-function member access
    - Extend macro expansion logic to properly handle field access syntax
    - Add test case demonstrating member access after function calls
    
    Example usage:
    ```c
    let value = pipe(&foo,
      (Foo_setA,(10)),
      (&Foo_setB,(20)->value),  // Member access after function call
      (deref,())
    );
    ```

--- dh/tests/mockup-pipe.c
+++ dh/tests/mockup-pipe.c
@@ -1,38 +1,64 @@
 #ifndef PIPE_INCLUDED
 #define PIPE_INCLUDED
 
-// Helper macros
-#define PIPE_EXPAND(...) __VA_ARGS__
-
 // Check if argument list is empty
 #define PIPE_HAS_ARGS(...) \
-    PIPE_HAS_ARGS_HELPER(__VA_OPT__(, ) __VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0) // 드디어 해냈다 ㅜㅜㅜㅜㅜ
-#define PIPE_HAS_ARGS_HELPER(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) \
-    N
+    PIPE_HAS_ARGS_IMPL(__VA_OPT__(, ) __VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0) // 드디어 해냈다 ㅜㅜㅜㅜㅜ
+#define PIPE_HAS_ARGS_IMPL(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
 
 // Conditional comma macro
 #define EMPTY()
-#define COMMA()                  ,
-#define PIPE_COMMA_IF(...)       PIPE_IF((PIPE_HAS_ARGS(__VA_ARGS__)), (COMMA), (EMPTY))
-#define PIPE_IF(cond, t, f)      PIPE_IF_IMPL(PIPE_EXPAND cond, t, f)
-#define PIPE_IF_IMPL(cond, t, f) pp_join(_, PIPE_IF, cond)(PIPE_EXPAND t, PIPE_EXPAND f)
-#define PIPE_IF_0(t, f)          f()
-#define PIPE_IF_1(t, f)          t()
+#define IGNORE(...)
+#define EXPAND(...) __VA_ARGS__
+#define DEFER(...)  __VA_ARGS__ EMPTY()
+#define COMMA()     ,
+
+// Helper macros
+#define PAREN_OPEN() (
+#define PAREN_CLOSE() )
+
+#define PIPE_EXPAND_WITH_PARENS_WRAPPED(...) (__VA_ARGS__)
+#define PIPE_EXPAND_WITH_PARENS_OPEN(...) (__VA_ARGS__
+#define PIPE_EXPAND_WITH_PARENS_CLOSE(...) __VA_ARGS__)
+
+#define IGNORE_OPEN() IGNORE(
+#define IGNORE_CLOSE() )
+#define COMMA_IGNORE_OPEN() , IGNORE(
+#define COMMA_IGNORE_CLOSE() )
+
+#define PIPE_exec(...) \
+    pp_join($, __PIPE_exec, __VA_ARGS__)
+#define __PIPE_exec$0comma_ignore_open \
+    , DEFER(IGNORE)(
+#define __PIPE_exec$0comma_ignore_close \
+    )
+#define __PIPE_exec$0ignore_open \
+    DEFER(IGNORE)(
+#define __PIPE_exec$0ignore_close \
+    )
+
+#define PIPE_REPLACE_COMMA_AND_IGNORE_IF(...) PIPE_IF((PIPE_HAS_ARGS(__VA_ARGS__)), 0comma_ignore_open, 0ignore_open)
+#define PIPE_IF(cond, t, f)                   PIPE_IF_IMPL(EXPAND cond, t, f)
+#define PIPE_IF_IMPL(cond, t, f)              pp_join(_, PIPE_IF, cond)(t, f)
+#define PIPE_IF_1(t, f)                       t
+#define PIPE_IF_0(t, f)                       f
 
 // Apply a function with a value and arguments
-#define PIPE_MAP(F, ARG...)              F(ARG)
-#define PIPE_APPLY(value, func, args...) PIPE_MAP(func, value PIPE_COMMA_IF args PIPE_EXPAND args)
+#define PIPE_MAP(F, ARG...)                     F ARG
+#define PIPE_JOIN_VALUE_TO_ARGS(value, args...) PIPE_EXPAND_WITH_PARENS_OPEN value PIPE_exec(PIPE_REPLACE_COMMA_AND_IGNORE_IF args) PIPE_exec(0comma_ignore_close) PIPE_EXPAND_WITH_PARENS_CLOSE args
+#define PIPE_APPLY(value, func, args...)        PIPE_MAP(func, PIPE_JOIN_VALUE_TO_ARGS(value, args))
 
 // Process a single step in the pipe
 #define PIPE_STEP(prev_result_var, step_num, func, args...) \
-    var ret##step_num = PIPE_APPLY(prev_result_var, func, args);
+    var ret##step_num = PIPE_APPLY((prev_result_var), func, args);
 
 // Generate a unique variable name for each step
 #define PIPE_RESULT(step_num) ret##step_num
 
 // Main pipe implementation
-#define pipe(initial_value, ...) ({           \
-    var __pipe_initial = (initial_value);     \
+#define pipe(initial_value...)             comp_syn__pipe(initial_value)
+#define comp_syn__pipe(initial_value, ...) ({ \
+    var __pipe_initial = initial_value;       \
     PIPE_IMPL(__pipe_initial, ##__VA_ARGS__); \
 })
 
@@ -48,68 +74,62 @@
 #define PIPE_COUNT_ARGS(arg1, arg2, arg3, arg4, arg5, func, ...) func
 
 // Implementation for different numbers of pipe steps
-#define PIPE1(initial_value, func_args1) ({                                         \
+#define PIPE1(initial_value, func_args1)                                            \
     PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1) \
-    PIPE_RESULT(0);                                                                 \
-})
-#define PIPE2(initial_value, func_args1, func_args2) ({                              \
+    PIPE_RESULT(0)
+#define PIPE2(initial_value, func_args1, func_args2)                                 \
     PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)  \
     PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2) \
-    PIPE_RESULT(1);                                                                  \
-})
-#define PIPE3(initial_value, func_args1, func_args2, func_args3) ({                  \
+    PIPE_RESULT(1)
+#define PIPE3(initial_value, func_args1, func_args2, func_args3)                     \
     PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)  \
     PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2) \
     PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC func_args3, PIPE_GET_ARGS func_args3) \
-    PIPE_RESULT(2);                                                                  \
-})
-#define PIPE4(initial_value, func_args1, func_args2, func_args3, func_args4) ({      \
+    PIPE_RESULT(2)
+#define PIPE4(initial_value, func_args1, func_args2, func_args3, func_args4)         \
     PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)  \
     PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2) \
     PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC func_args3, PIPE_GET_ARGS func_args3) \
     PIPE_STEP(PIPE_RESULT(2), 3, PIPE_GET_FUNC func_args4, PIPE_GET_ARGS func_args4) \
-    PIPE_RESULT(3);                                                                  \
-})
-#define PIPE5(initial_value, func_args1, func_args2, func_args3, func_args4, func_args5) ({ \
-    PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)         \
-    PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2)        \
-    PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC func_args3, PIPE_GET_ARGS func_args3)        \
-    PIPE_STEP(PIPE_RESULT(2), 3, PIPE_GET_FUNC func_args4, PIPE_GET_ARGS func_args4)        \
-    PIPE_STEP(PIPE_RESULT(3), 4, PIPE_GET_FUNC func_args5, PIPE_GET_ARGS func_args5)        \
-    PIPE_RESULT(4);                                                                         \
-})
-#define PIPE6(initial_value, func_args1, func_args2, func_args3, func_args4, func_args5, func_args6) ({ \
-    PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)                     \
-    PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2)                    \
-    PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC func_args3, PIPE_GET_ARGS func_args3)                    \
-    PIPE_STEP(PIPE_RESULT(2), 3, PIPE_GET_FUNC func_args4, PIPE_GET_ARGS func_args4)                    \
-    PIPE_STEP(PIPE_RESULT(3), 4, PIPE_GET_FUNC func_args5, PIPE_GET_ARGS func_args5)                    \
-    PIPE_STEP(PIPE_RESULT(4), 5, PIPE_GET_FUNC func_args6, PIPE_GET_ARGS func_args6)                    \
-    PIPE_RESULT(5);                                                                                     \
-})
-#define PIPE7(initial_value, func_args1, func_args2, func_args3, func_args4, func_args5, func_args6, func_args7) ({ \
-    PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)                                 \
-    PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2)                                \
-    PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC func_args3, PIPE_GET_ARGS func_args3)                                \
-    PIPE_STEP(PIPE_RESULT(2), 3, PIPE_GET_FUNC func_args4, PIPE_GET_ARGS func_args4)                                \
-    PIPE_STEP(PIPE_RESULT(3), 4, PIPE_GET_FUNC func_args5, PIPE_GET_ARGS func_args5)                                \
-    PIPE_STEP(PIPE_RESULT(4), 5, PIPE_GET_FUNC func_args6, PIPE_GET_ARGS func_args6)                                \
-    PIPE_STEP(PIPE_RESULT(5), 6, PIPE_GET_FUNC func_args7, PIPE_GET_ARGS func_args7)                                \
-    PIPE_RESULT(6);                                                                                                 \
-})
-#define PIPE8(initial_value, func_args1, func_args2, func_args3, func_args4, func_args5, func_args6, func_args7, func_args8) ({ \
-    PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)                                             \
-    PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2)                                            \
-    PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC func_args3, PIPE_GET_ARGS func_args3)                                            \
-    PIPE_STEP(PIPE_RESULT(2), 3, PIPE_GET_FUNC func_args4, PIPE_GET_ARGS func_args4)                                            \
-    PIPE_STEP(PIPE_RESULT(3), 4, PIPE_GET_FUNC func_args5, PIPE_GET_ARGS func_args5)                                            \
-    PIPE_STEP(PIPE_RESULT(4), 5, PIPE_GET_FUNC func_args6, PIPE_GET_ARGS func_args6)                                            \
-    PIPE_STEP(PIPE_RESULT(5), 6, PIPE_GET_FUNC func_args7, PIPE_GET_ARGS func_args7)                                            \
-    PIPE_STEP(PIPE_RESULT(6), 7, PIPE_GET_FUNC func_args8, PIPE_GET_ARGS func_args8)                                            \
-    PIPE_RESULT(7);                                                                                                             \
-})
+    PIPE_RESULT(3)
+#define PIPE5(initial_value, func_args1, func_args2, func_args3, func_args4, func_args5) \
+    PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)      \
+    PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2)     \
+    PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC func_args3, PIPE_GET_ARGS func_args3)     \
+    PIPE_STEP(PIPE_RESULT(2), 3, PIPE_GET_FUNC func_args4, PIPE_GET_ARGS func_args4)     \
+    PIPE_STEP(PIPE_RESULT(3), 4, PIPE_GET_FUNC func_args5, PIPE_GET_ARGS func_args5)     \
+    PIPE_RESULT(4)
+#define PIPE6(initial_value, func_args1, func_args2, func_args3, func_args4, func_args5, func_args6) \
+    PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)                  \
+    PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2)                 \
+    PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC func_args3, PIPE_GET_ARGS func_args3)                 \
+    PIPE_STEP(PIPE_RESULT(2), 3, PIPE_GET_FUNC func_args4, PIPE_GET_ARGS func_args4)                 \
+    PIPE_STEP(PIPE_RESULT(3), 4, PIPE_GET_FUNC func_args5, PIPE_GET_ARGS func_args5)                 \
+    PIPE_STEP(PIPE_RESULT(4), 5, PIPE_GET_FUNC func_args6, PIPE_GET_ARGS func_args6)                 \
+    PIPE_RESULT(5)
+#define PIPE7(initial_value, func_args1, func_args2, func_args3, func_args4, func_args5, func_args6, func_args7) \
+    PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)                              \
+    PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2)                             \
+    PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC func_args3, PIPE_GET_ARGS func_args3)                             \
+    PIPE_STEP(PIPE_RESULT(2), 3, PIPE_GET_FUNC func_args4, PIPE_GET_ARGS func_args4)                             \
+    PIPE_STEP(PIPE_RESULT(3), 4, PIPE_GET_FUNC func_args5, PIPE_GET_ARGS func_args5)                             \
+    PIPE_STEP(PIPE_RESULT(4), 5, PIPE_GET_FUNC func_args6, PIPE_GET_ARGS func_args6)                             \
+    PIPE_STEP(PIPE_RESULT(5), 6, PIPE_GET_FUNC func_args7, PIPE_GET_ARGS func_args7)                             \
+    PIPE_RESULT(6)
+#define PIPE8(initial_value, func_args1, func_args2, func_args3, func_args4, func_args5, func_args6, func_args7, func_args8) \
+    PIPE_STEP(initial_value, 0, PIPE_GET_FUNC func_args1, PIPE_GET_ARGS func_args1)                                          \
+    PIPE_STEP(PIPE_RESULT(0), 1, PIPE_GET_FUNC func_args2, PIPE_GET_ARGS func_args2)                                         \
+    PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC func_args3, PIPE_GET_ARGS func_args3)                                         \
+    PIPE_STEP(PIPE_RESULT(2), 3, PIPE_GET_FUNC func_args4, PIPE_GET_ARGS func_args4)                                         \
+    PIPE_STEP(PIPE_RESULT(3), 4, PIPE_GET_FUNC func_args5, PIPE_GET_ARGS func_args5)                                         \
+    PIPE_STEP(PIPE_RESULT(4), 5, PIPE_GET_FUNC func_args6, PIPE_GET_ARGS func_args6)                                         \
+    PIPE_STEP(PIPE_RESULT(5), 6, PIPE_GET_FUNC func_args7, PIPE_GET_ARGS func_args7)                                         \
+    PIPE_STEP(PIPE_RESULT(6), 7, PIPE_GET_FUNC func_args8, PIPE_GET_ARGS func_args8)                                         \
+    PIPE_RESULT(7)
 
 #endif /* PIPE_INCLUDED */
+
+
 
 #include "dh/main.h"
 #include "dh/heap/Page.h"
@@ -131,55 +151,97 @@ static fn_(Foo_eval(Foo* self), Foo*);
 static fn_(Foo_merge(Foo* self, const Foo* other), Foo*);
 static fn_(Foo_baz(const Foo* self), i32);
 
+static fn_(i32_add(i32 lhs, i32 rhs), i32) { return lhs + rhs; }
+static fn_(i32_addAsg(i32* lhs, i32 rhs), i32*) { return *lhs += rhs, lhs; }
+
+
+
+fn_(enhancePipeExpanding(Foo foo), void) {
+    // PIPE_STEP(PIPE_RESULT(1), 2, PIPE_GET_FUNC (deref_,().value), PIPE_GET_ARGS (deref_,().value))
+    // PIPE_STEP(PIPE_RESULT(1), 2, deref_, ().value)
+    // var ret1 = 0;
+    // var ret2 = PIPE_APPLY((ret1), deref_, ().value);
+    // PIPE_MAP(deref_, PIPE_EXPAND_WITH_PARENS_OPEN(ret1) PIPE_REPLACE_COMMA_AND_IGNORE_IF().value PAREN_CLOSE() PIPE_EXPAND_WITH_PARENS_CLOSE().value)
+    // PIPE_MAP(deref_, (ret1 IGNORE(.value)).value)
+    // PIPE_MAP(deref_, (ret1.value).value)
+
+    let value = pipe(&foo,(Foo_setA,(10)),(&Foo_setB,(20)->value),(i32_addAsg,(123)),(deref,()));
+    printf("Enhanced pipe result: %d\n", value);
+}
+
+/* fn_(resolveIssuePipeExpanding(mem_Allocator allocator), void) {
+    // #define try   try_
+    // #define catch catch_
+
+    // #define MAP(F, ...) F(__VA_ARGS__)
+    //     MAP(catch, value PIPE_REPLACE_COMMA_AND_IGNORE_IF(a) EXPAND(a))
+
+    //     PIPE_APPLY(ret, catch, (a));
+    //     PIPE_STEP(ret, 0, catch, (a));
+    //     var foo = meta_cast$(Foo*, pipe(pipe(allocator, (mem_Allocator_create,(typeInfo$(Foo)))), (catch,(a)), (abc,(def))));
+    //     var foo = meta_cast$(Foo*, catch_(mem_Allocator_create(allocator, typeInfo$(Foo)), claim_unreachable));
+
+    // let pipe_ret = pipe(allocator,(mem_Allocator_create,(typeInfo$(Foo))),(catch_from,(err, ({ $ignore err; claim_unreachable;}))));
+    // var foo      = meta_cast$(Foo*, pipe_ret);
+    // $ignore foo;
+
+    let pipe_foo = pipe(pipe(meta_cast$(Foo*, pipe(allocator,
+        (mem_Allocator_create,(typeInfo$(Foo))),(catch_,(claim_unreachable))
+    )),(deref,())),(ref,()),
+        (Foo_setA,(10)),
+        (Foo_setB,(20)),
+        (Foo_eval,()),
+        (Foo_baz,())
+    );
+    $ignore pipe_foo;
+} */
+
+
+
 fn_scope_ext(dh_main(Sli$Str_const args), Err$void) {
     $ignore args;
     let allocator = heap_Page_allocator(create$(heap_Page));
 
     // Traditional approach
     block_defer {
-        let foo1 = Foo_init(allocator);
-        defer_(Foo_fini(foo1));
-        {
-            Foo_setA(foo1, 10);
-            Foo_setB(foo1, 20);
-            Foo_eval(foo1);
-        }
-
         let bar = Foo_init(allocator);
-        defer_(Foo_fini(bar));
         {
             Foo_setA(bar, 5);
             Foo_setB(bar, 15);
             Foo_eval(bar);
         }
+        defer_(Foo_fini(bar));
 
-        Foo_merge(foo1, bar);
-        let result1 = Foo_baz(foo1);
+        let foo = Foo_init(allocator);
+        defer_(Foo_fini(foo));
+        Foo_setA(foo, 10);
+        Foo_setB(foo, 20);
+        Foo_eval(foo);
+        Foo_merge(foo, bar);
+        let result = Foo_baz(foo);
 
-        printf("Traditional result: %d\n", result1);
+        printf("Traditional result: %d\n", result);
     } block_deferral;
 
     // Using pipe macro
     block_defer {
-        let foo2 = Foo_init(allocator);
-        defer_(Foo_fini(foo2));
+        let bar = pipe(Foo_init(allocator),
+            (Foo_setA,(5)),
+            (Foo_setB,(15)),
+            (Foo_eval,())
+        );
+        defer_(Foo_fini(bar));
 
-        let bar2 = Foo_init(allocator);
-        defer_(Foo_fini(bar2));
-        {
-            Foo_setA(bar2, 5);
-            Foo_setB(bar2, 15);
-            Foo_eval(bar2);
-        }
-
-        let result2 = pipe(foo2,
+        let foo = Foo_init(allocator);
+        defer_(Foo_fini(foo));
+        let result = pipe(foo,
             (Foo_setA,(10)),
             (Foo_setB,(20)),
             (Foo_eval,()),
-            (Foo_merge,(bar2)),
+            (Foo_merge,(bar)),
             (Foo_baz,())
         );
-        printf("Pipe result: %d\n", result2);
+        printf("Pipe result: %d\n", result);
     } block_deferral;
 
     return_ok({});
@@ -187,7 +249,9 @@ fn_scope_ext(dh_main(Sli$Str_const args), Err$void) {
 
 // Example functions that would typically be used in a chain
 fn_(Foo_init(mem_Allocator allocator), Foo*) {
-    let foo    = meta_cast$(Foo*, catch_(mem_Allocator_create(allocator, typeInfo$(Foo)), claim_unreachable));
+    let foo    = meta_cast$(Foo*,
+        pipe(allocator,(mem_Allocator_create,(typeInfo$(Foo))),(catch_,(claim_unreachable)))
+    );
     foo->mem   = allocator;
     foo->a     = 0;
     foo->b     = 0;
@@ -215,26 +279,4 @@ fn_(Foo_merge(Foo* self, const Foo* other), Foo*) {
 }
 fn_(Foo_baz(const Foo* self), i32) {
     return self->value;
-}
-
-void resolveIssuePipeExpanding(mem_Allocator allocator) {
-    // #define try   try_
-    // #define catch catch_
-
-    // #define MAP(F, ...) F(__VA_ARGS__)
-    //     MAP(catch, value PIPE_COMMA_IF(a) PIPE_EXPAND(a))
-
-    //     PIPE_APPLY(ret, catch, (a));
-    //     PIPE_STEP(ret, 0, catch, (a));
-    //     var foo = meta_cast$(Foo*, pipe(pipe(allocator, (mem_Allocator_create,(typeInfo$(Foo)))), (catch,(a)), (abc,(def))));
-    //     var foo = meta_cast$(Foo*, catch_(mem_Allocator_create(allocator, typeInfo$(Foo)), claim_unreachable));
-
-    // let pipe_ret = pipe(allocator,(mem_Allocator_create,(typeInfo$(Foo))),(catch_from,(err, ({ $ignore err; claim_unreachable;}))));
-    // var foo      = meta_cast$(Foo*, pipe_ret);
-    // $ignore foo;
-
-    let pipe_foo = meta_cast$(Foo*, pipe(allocator,
-        (mem_Allocator_create,(typeInfo$(Foo))),(catch_,(claim_unreachable))
-    ));
-    $ignore pipe_foo;
 }
