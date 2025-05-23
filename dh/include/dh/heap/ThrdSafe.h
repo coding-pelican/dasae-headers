edea02ac4e59e6fe498d21b8bf9bc5bdb09fbaf2
Gyeongtae Kim <codingpelican@gmail.com>
2025-05-23 17:13:41 +0900

    feat(heap): Implement thread-safe memory allocator
    - Implement allocator interface in `heap_ThrdSafe.c`, providing functions for allocation, resizing, remapping, and freeing memory while ensuring thread safety through mutex locking

---
+++ dh/include/dh/heap/ThrdSafe.h
@@ -0,0 +1,38 @@
+/**
+ * @copyright Copyright (c) 2025 Gyeongtae Kim
+ * @license   MIT License - see LICENSE file for details
+ *
+ * @file    ThrdSafe.h
+ * @author  Gyeongtae Kim(dev-dasae) <codingpelican@gmail.com>
+ * @date    2025-05-21 (date of creation)
+ * @updated 2025-05-21 (date of last update)
+ * @version v0.1-alpha.1
+ * @ingroup dasae-headers(dh)/heap
+ * @prefix  heap_ThrdSafe
+ */
+
+#ifndef HEAP_THRD_SAFE_INCLUDED
+#define HEAP_THRD_SAFE_INCLUDED (1)
+#if defined(__cplusplus)
+extern "C" {
+#endif /* defined(__cplusplus) */
+
+/*========== Includes =======================================================*/
+
+#include "cfg.h"
+#include "dh/Thrd/Mtx.h"
+
+/*========== Page Allocator =================================================*/
+
+typedef struct heap_ThrdSafe {
+    mem_Allocator child_allocator;
+    Thrd_Mtx      mutex;
+} heap_ThrdSafe;
+
+/// Get allocator interface for instance
+extern fn_(heap_ThrdSafe_allocator(heap_ThrdSafe* self), mem_Allocator);
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif /* defined(__cplusplus) */
+#endif /* HEAP_THRD_SAFE_INCLUDED */
---
+++ dh/src/heap_ThrdSafe.c
@@ -0,0 +1,54 @@
+#include "dh/heap/ThrdSafe.h"
+
+// Forward declarations for allocator vtable functions
+static fn_(heap_ThrdSafe_alloc(anyptr ctx, usize len, u32 align), Opt$Ptr$u8);
+static fn_(heap_ThrdSafe_resize(anyptr ctx, Sli$u8 buf, u32 buf_align, usize new_len), bool);
+static fn_(heap_ThrdSafe_remap(anyptr ctx, Sli$u8 buf, u32 buf_align, usize new_len), Opt$Ptr$u8);
+static fn_(heap_ThrdSafe_free(anyptr ctx, Sli$u8 buf, u32 buf_align), void);
+
+fn_(heap_ThrdSafe_allocator(heap_ThrdSafe* self), mem_Allocator) {
+    debug_assert_nonnull(self);
+    // VTable for Thread-safe allocator
+    static const mem_Allocator_VT vt[1] = { {
+        .alloc  = heap_ThrdSafe_alloc,
+        .resize = heap_ThrdSafe_resize,
+        .remap  = heap_ThrdSafe_remap,
+        .free   = heap_ThrdSafe_free,
+    } };
+    return (mem_Allocator){
+        .ptr = self,
+        .vt  = vt
+    };
+}
+
+fn_scope_ext(heap_ThrdSafe_alloc(anyptr ctx, usize len, u32 align), Opt$Ptr$u8) {
+    debug_assert_nonnull(ctx);
+    let self = as$(heap_ThrdSafe*, ctx);
+    Thrd_Mtx_lock(&self->mutex);
+    defer_(Thrd_Mtx_unlock(&self->mutex));
+    return_(mem_Allocator_rawAlloc(self->child_allocator, len, align));
+} unscoped_ext;
+
+fn_scope_ext(heap_ThrdSafe_resize(anyptr ctx, Sli$u8 buf, u32 buf_align, usize new_len), bool) {
+    debug_assert_nonnull(ctx);
+    let self = as$(heap_ThrdSafe*, ctx);
+    Thrd_Mtx_lock(&self->mutex);
+    defer_(Thrd_Mtx_unlock(&self->mutex));
+    return_(mem_Allocator_rawResize(self->child_allocator, buf, buf_align, new_len));
+} unscoped_ext;
+
+fn_scope_ext(heap_ThrdSafe_remap(anyptr ctx, Sli$u8 buf, u32 buf_align, usize new_len), Opt$Ptr$u8) {
+    debug_assert_nonnull(ctx);
+    let self = as$(heap_ThrdSafe*, ctx);
+    Thrd_Mtx_lock(&self->mutex);
+    defer_(Thrd_Mtx_unlock(&self->mutex));
+    return_(mem_Allocator_rawRemap(self->child_allocator, buf, buf_align, new_len));
+} unscoped_ext;
+
+fn_scope_ext(heap_ThrdSafe_free(anyptr ctx, Sli$u8 buf, u32 buf_align), void) {
+    debug_assert_nonnull(ctx);
+    let self = as$(heap_ThrdSafe*, ctx);
+    Thrd_Mtx_lock(&self->mutex);
+    defer_(Thrd_Mtx_unlock(&self->mutex));
+    return_void(mem_Allocator_rawFree(self->child_allocator, buf, buf_align));
+} unscoped_ext;
