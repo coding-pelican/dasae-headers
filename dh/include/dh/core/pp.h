4da6c9be2570c1800353b6beea584174389026e8
Gyeongtae Kim <codingpelican@gmail.com>
2025-05-23 15:16:38 +0900

    feat(pp): Add new preprocessor utilities and update existing headers
    - Add `common.h` and `Tuple.h` for enhanced preprocessor utilities, including tuple manipulation and element access
    - Update `pp.h` to include new headers and reflect changes in versioning and grouping
    - Ensure consistency in naming conventions and maintain coherence across preprocessor utilities

--- dh/include/dh/core/pp.h
+++ dh/include/dh/core/pp.h
@@ -5,18 +5,10 @@
  * @file    pp.h
  * @author  Gyeongtae Kim(dev-dasae) <codingpelican@gmail.com>
  * @date    2024-11-05 (date of creation)
- * @updated 2025-02-01 (date of last update)
- * @version v0.1-alpha.1
- * @ingroup dasae-headers(dh)/core
+ * @updated 2025-05-23 (date of last update)
+ * @version v0.1-alpha.2
+ * @ingroup dasae-headers(dh)/core/pp
  * @prefix  pp
- *
- * @brief   Preprocessor utilities
- * @details Provides preprocessor utilities and functions for:
- *          - Stringification
- *          - Token concatenation
- *          - Token uniquification
- *          - Token counting
- *          - Token iteration
  */
 
 #ifndef CORE_PP_INCLUDED
@@ -25,133 +17,10 @@
 extern "C" {
 #endif /* defined(__cplusplus) */
 
-/*========== Macros and Definitions =========================================*/
+/*========== Includes =======================================================*/
 
-#define pp_nothing(...) comp_op__pp_nothing(__VA_ARGS__)
-
-#define pp_stringify(_Tok...) comp_op__pp_stringify(_Tok)
-#define nameOf(_Tok...)       comp_op__nameOf(_Tok)
-
-#define pp_cat(_Tok_lhs, _Tok_rhs...) \
-    comp_op__pp_cat(_Tok_lhs, _Tok_rhs)
-#define pp_cat2(_Tok_lhs, _Tok_rhs...) \
-    comp_op__pp_cat2(_Tok_lhs, _Tok_rhs)
-#define pp_cat3(_Tok_lhs, _Tok_mid, _Tok_rhs...) \
-    comp_op__pp_cat3(_Tok_lhs, _Tok_mid, _Tok_rhs)
-
-#define pp_join(_Tok_sep, _Tok_lhs, _Tok_rhs...) \
-    comp_op__pp_join(_Tok_sep, _Tok_lhs, _Tok_rhs)
-#define pp_join2(_Tok_sep, _Tok_lhs, _Tok_rhs...) \
-    comp_op__pp_join2(_Tok_sep, _Tok_lhs, _Tok_rhs)
-#define pp_join3(_Tok_sep, _Tok_lhs, _Tok_mid, _Tok_rhs...) \
-    comp_op__pp_join3(_Tok_sep, _Tok_lhs, _Tok_mid, _Tok_rhs)
-
-#define pp_uniqTok(_Tok...) \
-    comp_op__pp_uniqTok(_Tok)
-#define pp_uniqTokByLine(_Tok...) \
-    comp_op__pp_uniqTokByLine(_Tok)
-
-// pp_countArgs -> literal int: [0, 16) (handles up to 16 arguments)
-#define pp_countArgs(_Args...) \
-    comp_op__pp_countArgs(_Args)
-
-// pp_foreach (handles up to 8 arguments)
-#define pp_foreach(macro, name, ...) \
-    comp_op__pp_foreach_(comp_op__pp_foreach_NARG(__VA_ARGS__), macro, name, __VA_ARGS__)
-
-// lit_num(11,644,473,600u) => 11644473600u
-#define lit_num(_Comma_Sep_Lits...) \
-    comp_syn__lit_num(_Comma_Sep_Lits)
-#define lit_num$(T, _Comma_Sep_Lits...) \
-    comp_syn__lit_num$(T, _Comma_Sep_Lits)
-
-/*========== Macros Implementation ==========================================*/
-
-#define comp_op__pp_nothing(...)
-#define comp_op__pp_stringify(_Tok...) #_Tok
-#define comp_op__nameOf(_Tok...)       ($unused(_Tok), #_Tok)
-
-#define comp_op__pp_cat(_Tok_lhs, _Tok_rhs...) \
-    _Tok_lhs##_Tok_rhs
-#define comp_op__pp_cat2(_Tok_lhs, _Tok_rhs...) \
-    _Tok_lhs##_Tok_rhs
-#define comp_op__pp_cat3(_Tok_lhs, _Tok_mid, _Tok_rhs...) \
-    _Tok_lhs##_Tok_mid##_Tok_rhs
-
-#define comp_op__pp_join(_Tok_sep, _Tok_lhs, _Tok_rhs...) \
-    _Tok_lhs##_Tok_sep##_Tok_rhs
-#define comp_op__pp_join2(_Tok_sep, _Tok_lhs, _Tok_rhs...) \
-    _Tok_lhs##_Tok_sep##_Tok_rhs
-#define comp_op__pp_join3(_Tok_sep, _Tok_lhs, _Tok_mid, _Tok_rhs...) \
-    _Tok_lhs##_Tok_sep##_Tok_mid##_Tok_sep##_Tok_rhs
-
-#define comp_op__pp_uniqTok(_Tok...) \
-    pp_join(_, pp_join3(_, _, __LINE__, __COUNTER__), _Tok)
-#define comp_op__pp_uniqTokByLine(_Tok...) \
-    pp_join(_, pp_join2(_, _, __LINE__), _Tok)
-
-#define comp_op__pp_countArgs(...)                                        \
-    comp_op__pp_countArgs__selectArgCountInListRseqN(                     \
-        __VA_OPT__(, ) __VA_ARGS__, comp_op__pp_countArgs__getListRseqN() \
-    )
-#define comp_op__pp_countArgs__selectArgCountInListRseqN(_Args...) \
-    comp_op__pp_countArgs__argN(_Args)
-#define comp_op__pp_countArgs__getListRseqN() \
-    15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
-#define comp_op__pp_countArgs__argN(                                              \
-    _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _N, ... \
-) _N
-
-// pp_foreach macro implementation (handles up to 16 arguments)
-#define comp_op__pp_foreach_(N, macro, name, ...) \
-    pp_join(__, comp_op__pp_foreach, N)(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach_NARG(...) \
-    comp_op__pp_foreach_NARG_(__VA_ARGS__, comp_op__pp_foreach_RSEQ_N())
-#define comp_op__pp_foreach_NARG_(...) \
-    comp_op__pp_foreach_ARG_N(__VA_ARGS__)
-#define comp_op__pp_foreach_ARG_N(                                                 \
-    _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _N, ... \
-) _N
-#define comp_op__pp_foreach_RSEQ_N() \
-    16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
-#define comp_op__pp_foreach__1(macro, name, x)       macro(name, x)
-#define comp_op__pp_foreach__2(macro, name, x, ...)  macro(name, x) comp_op__pp_foreach__1(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__3(macro, name, x, ...)  macro(name, x) comp_op__pp_foreach__2(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__4(macro, name, x, ...)  macro(name, x) comp_op__pp_foreach__3(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__5(macro, name, x, ...)  macro(name, x) comp_op__pp_foreach__4(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__6(macro, name, x, ...)  macro(name, x) comp_op__pp_foreach__5(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__7(macro, name, x, ...)  macro(name, x) comp_op__pp_foreach__6(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__8(macro, name, x, ...)  macro(name, x) comp_op__pp_foreach__7(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__9(macro, name, x, ...)  macro(name, x) comp_op__pp_foreach__8(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__10(macro, name, x, ...) macro(name, x) comp_op__pp_foreach__9(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__11(macro, name, x, ...) macro(name, x) comp_op__pp_foreach__10(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__12(macro, name, x, ...) macro(name, x) comp_op__pp_foreach__11(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__13(macro, name, x, ...) macro(name, x) comp_op__pp_foreach__12(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__14(macro, name, x, ...) macro(name, x) comp_op__pp_foreach__13(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__15(macro, name, x, ...) macro(name, x) comp_op__pp_foreach__14(macro, name, __VA_ARGS__)
-#define comp_op__pp_foreach__16(macro, name, x, ...) macro(name, x) comp_op__pp_foreach__15(macro, name, __VA_ARGS__)
-
-#define comp_syn__lit_num(_Comma_Sep_Lits...) \
-    pp_join(__, comp_syn__lit_num, pp_countArgs(_Comma_Sep_Lits))(_Comma_Sep_Lits)
-/* Handle different numbers of arguments */
-#define comp_syn__lit_num__1(_Num1) \
-    _Num1
-#define comp_syn__lit_num__2(_Num1, _Num2) \
-    pp_cat(_Num1, _Num2)
-#define comp_syn__lit_num__3(_Num1, _Num2, _Num3) \
-    pp_cat3(_Num1, _Num2, _Num3)
-#define comp_syn__lit_num__4(_Num1, _Num2, _Num3, _Num4) \
-    pp_cat(pp_cat3(_Num1, _Num2, _Num3), _Num4)
-#define comp_syn__lit_num__5(_Num1, _Num2, _Num3, _Num4, _Num5) \
-    pp_cat(pp_cat(pp_cat3(_Num1, _Num2, _Num3), _Num4), _Num5)
-#define comp_syn__lit_num__6(_Num1, _Num2, _Num3, _Num4, _Num5, _Num6) \
-    pp_cat(pp_cat(pp_cat3(_Num1, _Num2, _Num3), pp_cat(_Num4, _Num5)), _Num6)
-#define comp_syn__lit_num__7(_Num1, _Num2, _Num3, _Num4, _Num5, _Num6, _Num7) \
-    pp_cat(pp_cat(pp_cat(pp_cat3(_Num1, _Num2, _Num3), pp_cat(_Num4, _Num5)), _Num6), _Num7)
-#define comp_syn__lit_num__8(_Num1, _Num2, _Num3, _Num4, _Num5, _Num6, _Num7, _Num8) \
-    pp_cat(pp_cat(pp_cat(pp_cat3(_Num1, _Num2, _Num3), pp_cat(_Num4, _Num5)), pp_cat(_Num6, _Num7)), _Num8)
-#define comp_syn__lit_num$(T, _Comma_Sep_Lits...) \
-    as$(T, lit_num(_Comma_Sep_Lits))
+#include "dh/core/pp/common.h"
+#include "dh/core/pp/Tuple.h"
 
 #if defined(__cplusplus)
 } /* extern "C" */
---
+++ dh/include/dh/core/pp/Tuple.h
@@ -0,0 +1,48 @@
+/**
+ * @copyright Copyright (c) 2025 Gyeongtae Kim
+ * @license   MIT License - see LICENSE file for details
+ *
+ * @file    Tuple.h
+ * @author  Gyeongtae Kim(dev-dasae) <codingpelican@gmail.com>
+ * @date    2025-05-23 (date of creation)
+ * @updated 2025-05-23 (date of last update)
+ * @version v0.1-alpha
+ * @ingroup dasae-headers(dh)/core/pp
+ * @prefix  pp_Tuple
+ *
+ * @brief   Preprocessor tuple utilities
+ * @details Provides preprocessor tuple utilities and functions for:
+ *          - Tuple wrapping and unwrapping
+ *          - Tuple element access
+ *          - Tuple element manipulation
+ *          - Tuple element counting
+ *          - Tuple element iteration
+ */
+
+#ifndef CORE_PP_TUPLE_INCLUDED
+#define CORE_PP_TUPLE_INCLUDED (1)
+#if defined(__cplusplus)
+extern "C" {
+#endif /* defined(__cplusplus) */
+
+/*========== Macros and Definitions =========================================*/
+
+#define pp_Tuple_wrap(...)   (__VA_ARGS__)
+#define pp_Tuple_unwrap(...) __VA_ARGS__
+
+#define pp_Tuple_get1st(_a, ...)                             _a
+#define pp_Tuple_get2nd(_a, _b, ...)                         _b
+#define pp_Tuple_get3rd(_a, _b, _c, ...)                     _c
+#define pp_Tuple_get4th(_a, _b, _c, _d, ...)                 _d
+#define pp_Tuple_get5th(_a, _b, _c, _d, _e, ...)             _e
+#define pp_Tuple_get6th(_a, _b, _c, _d, _e, _f, ...)         _f
+#define pp_Tuple_get7th(_a, _b, _c, _d, _e, _f, _g, ...)     _g
+#define pp_Tuple_get8th(_a, _b, _c, _d, _e, _f, _g, _h, ...) _h
+
+#define pp_Tuple_pushFront(_Elem, _Tuple) pp_Tuple_wrap(_Elem, pp_Tuple_unwrap _Tuple)
+#define pp_Tuple_pushBack(_Elem, _Tuple)  pp_Tuple_wrap(pp_Tuple_unwrap _Tuple, _Elem)
+
+#if defined(__cplusplus)
+}
+#endif /* defined(__cplusplus) */
+#endif /* CORE_PP_TUPLE_INCLUDED */
