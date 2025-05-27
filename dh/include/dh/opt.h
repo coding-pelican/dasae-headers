5b627c26caa73c3e6d9c3577984e322e9777107a
Gyeongtae Kim <codingpelican@gmail.com>
2025-05-27 19:39:26 +0900

    refactor(err_res, opt): Standardize formatting

--- dh/include/dh/opt.h
+++ dh/include/dh/opt.h
@@ -78,45 +78,45 @@ extern "C" {
 /*========== Implementations ================================================*/
 
 #define comp_gen__use_Opt$(T) \
-    use_Ptr$(T);              \
-    decl_Opt$(T);             \
+    use_Ptr$(T); \
+    decl_Opt$(T); \
     impl_Opt$(T)
-#define comp_gen__decl_Opt$(T)                                            \
+#define comp_gen__decl_Opt$(T) \
     $maybe_unused typedef struct Opt$(Ptr_const$(T)) Opt$(Ptr_const$(T)); \
-    $maybe_unused typedef struct Opt$(Ptr$(T)) Opt$(Ptr$(T));             \
+    $maybe_unused typedef struct Opt$(Ptr$(T)) Opt$(Ptr$(T)); \
     $maybe_unused typedef struct Opt$(T) Opt$(T)
-#define comp_gen__impl_Opt$(T)   \
+#define comp_gen__impl_Opt$(T) \
     struct Opt$(Ptr_const$(T)) { \
-        rawptr_const$(T) value;  \
-        bool has_value;          \
-    };                           \
-    struct Opt$(Ptr$(T)) {       \
-        rawptr$(T) value;        \
-        bool has_value;          \
-    };                           \
-    struct Opt$(T) {             \
-        T    value;              \
-        bool has_value;          \
+        rawptr_const$(T) value; \
+        bool has_value; \
+    }; \
+    struct Opt$(Ptr$(T)) { \
+        rawptr$(T) value; \
+        bool has_value; \
+    }; \
+    struct Opt$(T) { \
+        T    value; \
+        bool has_value; \
     }
 
 #define comp_type_alias__Opt$(T) \
     pp_join($, Opt, T)
 #define comp_type_anon__Opt$$(T) \
-    struct {                     \
-        T    value;              \
-        bool has_value;          \
+    struct { \
+        T    value; \
+        bool has_value; \
     }
-#define comp_op__Opt_anonCast$(T_Opt, var_anon...) eval({                                           \
-    let __anon = var_anon;                                                                          \
-    claim_assert_static(sizeOf(TypeOf(__anon)) == sizeOf(T_Opt));                                   \
-    claim_assert_static(alignOf(TypeOf(__anon)) == alignOf(T_Opt));                                 \
-    claim_assert_static(hasField(TypeOf(__anon), has_value));                                       \
-    claim_assert_static(validateField(TypeOf(__anon), has_value, FieldType$(T_Opt, has_value)));    \
+#define comp_op__Opt_anonCast$(T_Opt, var_anon...) eval({ \
+    let __anon = var_anon; \
+    claim_assert_static(sizeOf(TypeOf(__anon)) == sizeOf(T_Opt)); \
+    claim_assert_static(alignOf(TypeOf(__anon)) == alignOf(T_Opt)); \
+    claim_assert_static(hasField(TypeOf(__anon), has_value)); \
+    claim_assert_static(validateField(TypeOf(__anon), has_value, FieldType$(T_Opt, has_value))); \
     claim_assert_static(fieldPadding(TypeOf(__anon), has_value) == fieldPadding(T_Opt, has_value)); \
-    claim_assert_static(hasField(TypeOf(__anon), value));                                           \
-    claim_assert_static(validateField(TypeOf(__anon), value, FieldType$(T_Opt, value)));            \
-    claim_assert_static(fieldPadding(TypeOf(__anon), value) == fieldPadding(T_Opt, value));         \
-    eval_return(*(T_Opt*)&__anon);                                                                  \
+    claim_assert_static(hasField(TypeOf(__anon), value)); \
+    claim_assert_static(validateField(TypeOf(__anon), value, FieldType$(T_Opt, value))); \
+    claim_assert_static(fieldPadding(TypeOf(__anon), value) == fieldPadding(T_Opt, value)); \
+    eval_return(*(T_Opt*)&__anon); \
 })
 
 #define comp_op__some(val_some...)         { .has_value = true, .value = val_some }
@@ -125,10 +125,10 @@ extern "C" {
 #define comp_op__none$(T_Opt)              ((T_Opt)none())
 
 #define comp_op__Opt_asg$(__addr_opt, T_Opt, var_addr_opt, val_opt...) eval({ \
-    let __addr_opt = var_addr_opt;                                            \
-    debug_assert_nonnull(__addr_opt);                                         \
-    *__addr_opt = *(T_Opt[1]){ [0] = val_opt };                               \
-    eval_return __addr_opt;                                                   \
+    let __addr_opt = var_addr_opt; \
+    debug_assert_nonnull(__addr_opt); \
+    *__addr_opt = *(T_Opt[1]){ [0] = val_opt }; \
+    eval_return __addr_opt; \
 })
 #define comp_op__Opt_asg(var_addr_opt, val_opt...)         Opt_asg$(TypeOf(*var_addr_opt), var_addr_opt, val_opt)
 #define comp_op__toSome$(T_Opt, var_addr_opt, val_some...) Opt_asg$(T_Opt, var_addr_opt, some(val_some))
@@ -143,34 +143,34 @@ extern "C" {
 #define comp_syn__return_none()            return_(none())
 
 #define comp_op__orelse(__result, _Expr, _Default_Or_Eval...) eval({ \
-    var __result = _Expr;                                            \
-    if (isNone(__result)) {                                          \
-        __result.value = bti_Generic_match$(                         \
-            TypeOf(_Default_Or_Eval),                                \
-            bti_Generic_pattern$(void) eval({                        \
-                $ignore_void _Default_Or_Eval;                       \
-                eval_return make$(TypeOf(__result.value));           \
-            }),                                                      \
-            bti_Generic_fallback_ _Default_Or_Eval                   \
-        );                                                           \
-    }                                                                \
-    eval_return __result.value;                                      \
+    var __result = _Expr; \
+    if (isNone(__result)) { \
+        __result.value = bti_Generic_match$( \
+            TypeOf(_Default_Or_Eval), \
+            bti_Generic_pattern$(void) eval({ \
+                $ignore_void _Default_Or_Eval; \
+                eval_return make$(TypeOf(__result.value)); \
+            }), \
+            bti_Generic_fallback_ _Default_Or_Eval \
+        ); \
+    } \
+    eval_return __result.value; \
 })
 #define comp_op__unwrap(_Expr...) orelse(_Expr, claim_unreachable)
 
 #define comp_op__Opt_asPtr$(__addr_opt, T_OptPtr, var_addr_opt...) eval({ \
-    let __addr_opt = var_addr_opt;                                        \
-    debug_assert_nonnull(__addr_opt);                                     \
-    (T_OptPtr){                                                           \
-        .value     = &__addr_opt->value,                                  \
-        .has_value = __addr_opt->has_value                                \
-    };                                                                    \
+    let __addr_opt = var_addr_opt; \
+    debug_assert_nonnull(__addr_opt); \
+    (T_OptPtr){ \
+        .value     = &__addr_opt->value, \
+        .has_value = __addr_opt->has_value \
+    }; \
 })
 #define comp_op__Opt_asPtr(var_addr_opt...) \
     Opt_asPtr$(Opt$$(FieldType$(TypeOf(*var_addr_opt), value)*), var_addr_opt)
 
 #define comp_syn__if_some(val_opt, _Payload_Capture...) \
-    if_(let _result = (val_opt), _result.has_value)     \
+    if_(let _result = (val_opt), _result.has_value) \
         with_(let _Payload_Capture = _result.value)
 #define comp_syn__else_none \
     else
@@ -178,7 +178,7 @@ extern "C" {
     if_(let _result = (val_opt), !_result.has_value)
 #define comp_syn__else_some(_Payload_Capture...) \
     else_(let _Payload_Capture = _result.value)
-#define comp_syn__while_some(val_opt, _Payload_Capture...)                \
+#define comp_syn__while_some(val_opt, _Payload_Capture...) \
     for (var _result = (val_opt); _result.has_value; _result = (val_opt)) \
     with_(let _Payload_Capture = _result.value)
 #define comp_syn__while_none(val_opt...) \
