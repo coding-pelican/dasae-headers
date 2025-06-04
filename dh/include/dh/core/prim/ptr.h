87409433a86573b8e5612055ca28424b9410d6ce
Gyeongtae Kim <codingpelican@gmail.com>
2025-06-04 13:38:47 +0900

    refactor(ptr): Rename pointer-related macros to use `comp_` prefix

--- dh/include/dh/core/prim/ptr.h
+++ dh/include/dh/core/prim/ptr.h
@@ -37,92 +37,94 @@ typedef const void* voidptr_const;
 typedef void*       voidptr;
 typedef ptrdiff_t   ptrdiff;
 
-#define anyptr_const                            \
-    /**                                         \
+#define anyptr_const \
+    /** \
      * @brief Pointer to any type (const void*) \
-     */                                         \
-    TYPE__anyptr_const
+     */ \
+    comp_prim_type__anyptr_const
 
-#define anyptr                            \
-    /**                                   \
+#define anyptr \
+    /** \
      * @brief Pointer to any type (void*) \
-     */                                   \
-    TYPE__anyptr
+     */ \
+    comp_prim_type__anyptr
 
-#define rawptr_const$(TSrc)                 \
-    /**                                     \
+#define rawptr_const$(TSrc) \
+    /** \
      * @brief Pointer to type (const TSrc*) \
-     */                                     \
-    TYPE__rawptr_const$(TSrc)
+     */ \
+    comp_prim_type__rawptr_const$(TSrc)
 
-#define rawptr$(TSrc)                 \
-    /**                               \
+#define rawptr$(TSrc) \
+    /** \
      * @brief Pointer to type (TSrc*) \
-     */                               \
-    TYPE__rawptr$(TSrc)
+     */ \
+    comp_prim_type__rawptr$(TSrc)
 
-#define rawptrCast$(TDestRawptr, var_rawptr)                \
-    /**                                                     \
+#define rawptrCast$(TDestRawptr, var_rawptr) \
+    /** \
      * @brief Convert anyptr to pointer of type TDestRawptr \
-     */                                                     \
-    FUNC__rawptrCast$(TDestRawptr, var_rawptr)
+     */ \
+    comp_op__rawptrCast$(TDestRawptr, var_rawptr)
 
-#define rawptrToInt(val_rawptr)            \
-    /**                                    \
+#define intFromRawptr rawptrToInt
+#define rawptrToInt(val_rawptr) \
+    /** \
      * @brief Convert rawptr to int(usize) \
-     */                                    \
-    FUNC__rawptrToInt(val_rawptr)
+     */ \
+    comp_op__rawptrToInt(val_rawptr)
 
+#define rawptrFromInt$ intToRawptr
 #define intToRawptr$(TDestRawptr, val_int) \
-    /**                                    \
-     * @brief Convert int to rawptr        \
-     */                                    \
-    FUNC__intToRawptr$(TDestRawptr, val_int)
+    /** \
+     * @brief Convert int to rawptr \
+     */ \
+    comp_op__intToRawptr$(TDestRawptr, val_int)
 
-#define rawptrIsNull(var_rawptr)       \
-    /**                                \
+#define rawptrIsNull(var_rawptr) \
+    /** \
      * @brief Check if pointer is null \
-     */                                \
-    FUNC__rawptrIsNull(var_rawptr)
+     */ \
+    comp_op__rawptrIsNull(var_rawptr)
 
-#define rawptrIsNonnull(var_rawptr)        \
-    /**                                    \
+#define rawptrIsNonnull(var_rawptr) \
+    /** \
      * @brief Check if pointer is non-null \
-     */                                    \
-    FUNC__rawptrIsNonnull(var_rawptr)
+     */ \
+    comp_op__rawptrIsNonnull(var_rawptr)
 
 #define ensureNonnull(val_ptr...) comp_op__ensureNonnull(pp_uniqTok(ptr), val_ptr)
 
-#define rawref(var_src...)   comp_syn__rawref(var_src)
-#define rawderef(val_ptr...) comp_syn__rawderef(val_ptr)
+#define rawref(var_src...)   comp_op__rawref(var_src)
+#define rawderef(val_ptr...) comp_op__rawderef(val_ptr)
 
-#define ref(val_src...)   comp_syn__ref(val_src)
-#define deref(val_ptr...) comp_syn__deref(val_ptr)
+#define ref(val_src...)   comp_op__ref(val_src)
+#define deref(val_ptr...) comp_op__deref(val_ptr)
 
 /*========== Macros Implementation ==========================================*/
 
-#define TYPE__anyptr_const        const void*
-#define TYPE__anyptr              void*
-#define TYPE__rawptr_const$(TSrc) const TSrc*
-#define TYPE__rawptr$(TSrc)       TSrc*
+#define comp_prim_type__anyptr_const        const void*
+#define comp_prim_type__anyptr              void*
+#define comp_prim_type__rawptr_const$(TSrc) const TSrc*
+#define comp_prim_type__rawptr$(TSrc)       TSrc*
 
-#define FUNC__rawptrCast$(TDestRawptr, var_rawptr) ((TDestRawptr)(var_rawptr))
-#define FUNC__rawptrToInt(val_rawptr)              ((usize)(val_rawptr))    // NOLINT
-#define FUNC__intToRawptr$(TDestRawptr, val_int)   ((TDestRawptr)(val_int)) // NOLINT
+#define comp_op__rawptrCast$(TDestRawptr, var_rawptr) ((TDestRawptr)(var_rawptr))
+#define comp_op__rawptrToInt(val_rawptr)              ((usize)(val_rawptr))    // NOLINT
+#define comp_op__intToRawptr$(TDestRawptr, val_int)   ((TDestRawptr)(val_int)) // NOLINT
 
-#define FUNC__rawptrIsNull(var_rawptr)    ((var_rawptr) == null)
-#define FUNC__rawptrIsNonnull(var_rawptr) ((var_rawptr) != null)
+#define comp_op__rawptrIsNull(var_rawptr)    ((var_rawptr) == null)
+#define comp_op__rawptrIsNonnull(var_rawptr) ((var_rawptr) != null)
 
 #define comp_op__ensureNonnull(__ptr, val_ptr...) eval({ \
-    TypeOf(*val_ptr)* __ptr = val_ptr;                   \
-    eval_return debug_assert_nonnull(__ptr), __ptr;      \
+    TypeOf(*val_ptr)* __ptr = val_ptr; \
+    eval_return debug_assert_nonnull(__ptr), __ptr; \
 })
 
-#define comp_syn__rawref(var_src...)   (&(var_src))
-#define comp_syn__rawderef(val_ptr...) (*(val_ptr))
+#define comp_op__rawref(var_src...)   (&(var_src))
+#define comp_op__rawderef(val_ptr...) (*(val_ptr))
 
-#define comp_syn__ref(val_src...)   (&(val_src))
-#define comp_syn__deref(val_ptr...) (*ensureNonnull(val_ptr))
+#define comp_op__ref(val_src...)   (&(val_src))
+#define comp_op__deref(val_ptr...) (*ensureNonnull(val_ptr))
 
 /*========== Validation Checks ==============================================*/
 
